411e7c2e81ccdbc466d9dbc133ab55226789172c
Luis Felipe Brum <brum.felipe@outlook.com>
2025-09-23 14:46:34 -0300

    feat: finish `/status` endpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -37,7 +37,11 @@ describe("GET /api/v1/status", () => {
     });
 
     test("should return database max_connections properly", () => {
-      expect(responseBody.dependencies.database.max_connections).toEqual("100");
+      expect(responseBody.dependencies.database.max_connections).toEqual(100);
+    });
+
+    test("should return database opened_connections properly", () => {
+      expect(responseBody.dependencies.database.opened_connections).toEqual(1);
     });
   });
 });
